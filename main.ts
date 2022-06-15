23b69a1b4de12940f2ae0bb1475366459dcf3f03
mkmulvi30 <98337534+mkmulvi30@users.noreply.github.com>
2022-06-15 10:28:36 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Hello!")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.Ghost)
+})
